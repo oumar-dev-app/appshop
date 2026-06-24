a0898603e282291e9525f5efdda10d64f134c7ff
oumar-dev-app <bourema1@icloud.com>
2026-06-24 16:40:34 +0000

    clean initial commit

--- _Components/HeaderDashboard.tsx
+++ _Components/HeaderDashboard.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use client";
 
 import { useEffect, useState } from "react";
@@ -21,7 +20,7 @@ function Header() {
     return (
         <header className="mx-4 sm:mx-6 lg:mx-8">
             <div className="max-w-7xl m-auto border border-white bg-white text-black rounded-lg shadow-lg mt-4 mb-2 py-4 px-4 sm:px-6 flex items-center justify-between">
-                
+
                 <h1 className="text-lg sm:text-2xl font-semibold">
                     Dashboard
                 </h1>
@@ -47,46 +46,41 @@ function Header() {
             </div>
         </header>
     );
-=======
-import { FaRegBell } from 'react-icons/fa';
-import Image from 'next/image';
-import Link from 'next/link';
 
-function Header() {
-    return (
-        <header className=' mx-4 sm:mx-6 lg:mx-8  '>
-            <div className='max-w-7xl m-auto border border-white bg-white text-black rounded-lg shadow-lg mt-4 mb-2  py-4 px-4 sm:px-6 flex items-center justify-between'>
-                <h1 className='text-lg sm:text-2xl font-semibold'>
-                    Dashboard
-                </h1>
+    function Header() {
+        return (
+            <header className=' mx-4 sm:mx-6 lg:mx-8  '>
+                <div className='max-w-7xl m-auto border border-white bg-white text-black rounded-lg shadow-lg mt-4 mb-2  py-4 px-4 sm:px-6 flex items-center justify-between'>
+                    <h1 className='text-lg sm:text-2xl font-semibold'>
+                        Dashboard
+                    </h1>
 
 
-                <div className='flex items-center space-x-3 sm:space-x-6'>
-                    {/*                     <div>
+                    <div className='flex items-center space-x-3 sm:space-x-6'>
+                        {/*                     <div>
                         <FaRegBell className='w-5 sm:w-5 h-5 sm:h-5 text-black hover:text-white cursor-pointer' />
                     </div> */}
 
-                    <div >
-                        <Link
-                            href={"/dashboard/reglage"}
-                            className='flex items-center space-x-2 sm:space-x-3'
-                        >
-                            <Image src={"/oumar.png"} alt='icon' width={40} height={40} className='rounded-full' />
+                        <div >
+                            <Link
+                                href={"/dashboard/reglage"}
+                                className='flex items-center space-x-2 sm:space-x-3'
+                            >
+                                <Image src={"/oumar.png"} alt='icon' width={40} height={40} className='rounded-full' />
 
 
 
-                            <span className='hidden sm:block text-black font-medium'>
-                                Oumar
-                            </span>
-                        </Link>
+                                <span className='hidden sm:block text-black font-medium'>
+                                    Oumar
+                                </span>
+                            </Link>
+                        </div>
+
                     </div>
-
                 </div>
-            </div>
 
-        </header>
-    )
->>>>>>> 6ddfba36 (first commit)
+            </header>
+        )
+    }
 }
-
 export default Header;
