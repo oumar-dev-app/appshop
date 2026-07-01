9d75a7a7362065ca5b9c24a6313cfeefd59ca77a
oumar-dev-app <bourema1@icloud.com>
2026-07-01 01:42:58 +0000

    modification au niveau du bouton retour dans la page panier

--- app/panier/page.tsx
+++ app/panier/page.tsx
@@ -75,7 +75,7 @@ export default function PanierPage() {
                 <div>
 
                     {/* TITRE */}
-                    <h1 className="text-2xl sm:text-3xl font-bold mb-4 sm:mb-6">
+                    <h1 className="text-2xl sm:text-3xl font-bold">
                         Mon panier
                     </h1>
                 </div>
